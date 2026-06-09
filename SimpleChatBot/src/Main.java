170887cbc4252809250c52ee9f5e2b2cd4bedd96
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-09 19:43:59 +0530

    push my chatbot

---
+++ SimpleChatBot/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        String name = "Jam";
+        int year = 2026;
+        System.out.println("Hello! My name is "+ name+".");
+        System.out.println("I was created in "+ year+".");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+
+        System.out.println("What a great name you have, {yourName}!");
+    }
+}
