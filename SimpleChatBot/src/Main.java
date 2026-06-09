c33d70c595c0f26f163925ba53d0bda538a7ebc8
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-09 20:21:59 +0530

    guessing age

--- SimpleChatBot/src/Main.java
+++ SimpleChatBot/src/Main.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        String name = "Jam";
+        Scanner sc = new Scanner(System.in);
+        String bot_name = "Jam";
         int year = 2026;
-        System.out.println("Hello! My name is "+ name+".");
+        System.out.println("Hello! My name is "+ bot_name+".");
         System.out.println("I was created in "+ year+".");
-        System.out.println("Please, remind me your name.");
+        System.out.print("Please, remind me your name.\n>");
 
         // reading a name
+        String name = sc.nextLine();
 
-        System.out.println("What a great name you have, {yourName}!");
+        System.out.println("What a great name you have, "+ name +"!");
+        System.out.println("Let me guess your age.");
+        System.out.print("Enter remainders of dividing your age by 3, 5 and 7.\n>");
+
+        // reading all remainders
+        int remainder3 = sc.nextInt();
+        System.out.print(">");
+        int remainder5 = sc.nextInt();
+        System.out.print(">");
+        int remainder7 = sc.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is "+age+" that's a good time to start programming!");
     }
 }
