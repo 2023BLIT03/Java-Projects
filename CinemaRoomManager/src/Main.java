388191af6e8915c365d011c5741d043fb470039f
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-11 22:44:23 +0530

    first commit

---
+++ CinemaRoomManager/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(){
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for(int i = 1; i <= 8; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
