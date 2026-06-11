b757bc8b11ede07aba8dd687a20519f82a26414b
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-11 17:55:15 +0530

    finalise my project

--- Zookeeper/src/Main.java
+++ Zookeeper/src/Main.java
@@ -1,12 +1,187 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner sc = new Scanner(System.in);
+        String n;
+        do{
+            System.out.print("Please enter the number of the habitat you would like to view: > ");
+            n = sc.next();
+            switch(n){
+                case "0":
+                    camel();
+                    break;
+                case "1":
+                    lion();
+                    break;
+                case "2":
+                    deer();
+                    break;
+                case "3":
+                    goose();
+                    break;
+                case "4":
+                    bat();
+                    break;
+                case "5":
+                    rabbit();
+                    break;
+                default:
+                    System.out.println("Invalid Number");
+            }
+
+        }while(!n.equals("exit"));
+        end();
+    }
+
+    static void camel(){
+        String camel = """
+                Switching on the camera in the camel habitat...
+                 ___.-''''-.
+                /___  @    |
+                ',,,,.     |         _.'''''''._
+                     '     |        /           \\
+                     |     \\    _.-'             \\
+                     |      '.-'                  '-.
+                     |                               ',
+                     |                                '',
+                      ',,-,                           ':;
+                           ',,| ;,,                 ,' ;;
+                              ! ; !'',,,',',,,,'!  ;   ;:
+                             : ;  ! !       ! ! ;  ;   :;
+                             ; ;   ! !      ! !  ; ;   ;,
+                            ; ;    ! !     ! !   ; ;
+                            ; ;    ! !    ! !     ; ;
+                           ;,,      !,!   !,!     ;,;
+                           /_I      L_I   L_I     /_I
+                Look at that! Our little camel is sunbathing!
+                """;
+        System.out.println(camel);
+    }
+
+    static void lion(){
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+        System.out.println(lion);
+    }
+
+    static void deer(){
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+        System.out.println(deer);
+    }
+
+    static void goose(){
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+        System.out.println(goose);
+    }
+
+    static void bat(){
+        String bat = """
+                Switching on the camera in the bat habitat...
+                _________________               _________________
+                 ~-.              \\  |\\___/|  /              .-~
+                     ~-.           \\ / o o \\ /           .-~
+                        >           \\\\  W  //           <
+                       /             /~---~\\             \\
+                      /_            |       |            _\\
+                         ~-.        |       |        .-~
+                            ;        \\     /        i
+                           /___      /\\   /\\      ___\\
+                                ~-. /  \\_/  \\ .-~
+                                   V         V
+                This bat looks like it's doing fine.
+                """;
+        System.out.println(bat);
+
+    }
+
+    static void rabbit(){
+        String rabbit = """
+                Switching on the camera in the rabbit habitat...
+                         ,
+                        /|      __
+                       / |   ,-~ /
+                      Y :|  //  /
+                      | jj /( .^
+                      >-"~"-v"
+                     /       Y
+                    jo  o    |
+                   ( ~T~     j
+                    >._-' _./
+                   /   "~"  |
+                  Y     _,  |
+                 /| ;-"~ _  l
+                / l/ ,-"~    \
+                \\//\\/      .- \
+                 Y        /    Y
+                 l       I     !
+                 ]\\      _\\    /"\
+                (" ~----( ~   Y.  )
+                It looks like we will soon have more rabbits!
+                """;
+        System.out.println(rabbit);
+
+    }
+
+    static void end(){
         System.out.println("""
-            I love animals!
-            Let's check on the animals...
-            The deer looks fine.
-            The bat looks happy.
-            The lion looks healthy.
-            """);
+                See you later!
+                """);
     }
 }
