4fdf8d953bc1c0a0cc62ef7d264622dbcd742812
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-11 22:53:22 +0530

    1st stage complete

--- CinemaRoomManager/src/Main.java
+++ CinemaRoomManager/src/Main.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("Cinema:");
-        System.out.print(" ");
+        System.out.print("  ");
         for(int i = 1; i <= 8; i++){
             System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int i = 1; i <= 7; i++){
+            System.out.print(i+" ");
+            for(int j = 1; j <= 8; j++){
+                System.out.print("S ");
+            }
+            System.out.println();
         }
     }
 }
