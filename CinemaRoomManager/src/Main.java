440b2aaf07fc911d87913c4694cab52396ac46af
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-12 23:53:23 +0530

    stage 3 began

--- CinemaRoomManager/src/Main.java
+++ CinemaRoomManager/src/Main.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 
 public class Main{
+    final static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows:\n> ");
         int r = sc.nextInt();
         System.out.print("Enter the number of seats in each row:\n> ");
         int s = sc.nextInt();
+        int option;
+        do{
+            System.out.print("""
+                    1. Show the seats
+                    2. Buy a ticket
+                    0. Exit""");
+            System.out.print("\n> ");
+            option = sc.nextInt();
+            if(option == 1){
+                seatingPlan(r, s);
+            } else if(option == 2){
+                book(r, s);
+                ticketPrice();
+            }else if(option > 2 || option < 0){
+                System.out.println("Invalid Number");
+            }
+        }while(option != 0);
+        /*
         System.out.println("Total income:");
         if(r * s <= 60){
             ticket(r,s);
@@ -16,11 +35,7 @@ public class Main{
             ticketOdd(r, s);
         }
         seatingPlan(r, s);
-        System.out.print("Enter a row number:\n> ");
-        int row_num = sc.nextInt();
-        System.out.print("Enter a seat number in that row:\n> ");
-        int seat_num = sc.nextInt();
-        newSeatingPlan(r, s, row_num, seat_num);
+         */
     }
 
     static void seatingPlan(int r, int s){
@@ -59,6 +74,14 @@ public class Main{
         }
     }
 
+    static void book(int r, int s){
+        System.out.print("Enter a row number:\n> ");
+        int row_num = sc.nextInt();
+        System.out.print("Enter a seat number in that row:\n> ");
+        int seat_num = sc.nextInt();
+        //newSeatingPlan(r, s, row_num, seat_num);
+    }
+
     static void ticket(int r, int s){
         int income = r*s*10;
         System.out.println("$"+income);
@@ -72,5 +95,9 @@ public class Main{
     static void ticketOdd(int r, int s){
         int income = (r/2 * s * 10) + ((r/2+1) * s * 8);
         System.out.println("$"+income);
+    }
+
+    static void ticketPrice(){
+        System.out.println("Ticket price: $10\n");
     }
 }
