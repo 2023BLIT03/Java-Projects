4a4a803e82d4bf463668a8d388b1df092681515d
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-13 22:45:15 +0530

    stage 3 completed

--- Last Pencil/src/Main.java
+++ Last Pencil/src/Main.java
@@ -5,15 +5,28 @@ class Main{
     public static void main(String[] args){
         //String pencil = sc.nextLine();
         System.out.print("How many pencils would you like to use:\n> ");
-        int count = sc.nextInt();
+        int pencil = sc.nextInt();
         sc.nextLine();
 
         System.out.print("Who will be the first (John, Jack):\n> ");
         String name = sc.nextLine();
-        for(int i=0; i<count; i++){
-            System.out.print("|");
-        }
-        System.out.println();
         System.out.println(name+ " is going first!");
+        int remove = 0;
+        do{
+            for(int i=0; i<pencil; i++){
+                System.out.print("|");
+            }
+            System.out.println();
+            if(name.equals("John")){
+                name = "Jack";
+                System.out.print("Jack's turn\n> ");
+            }else{
+                System.out.print("John's turn\n> ");
+                name = "John";
+            }
+            remove = sc.nextInt();
+            pencil -= remove;
+        }while(pencil!=0);
+
     }
 }
