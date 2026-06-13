07c76ac2eb56456f6e1d384e9f79c6113d579f3f
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-13 20:11:07 +0530

    lets began

---
+++ Last Pencil/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main{
+    final static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        //String pencil = sc.nextLine();
+        System.out.println("||||");
+        System.out.println("Your turn!");
+    }
+}
