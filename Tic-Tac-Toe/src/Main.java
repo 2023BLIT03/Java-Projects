84f652f380281edb7ea0b1c8ae497b6595c0c43f
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-14 21:50:32 +0530

    stage 2 completed

--- Tic-Tac-Toe/src/Main.java
+++ Tic-Tac-Toe/src/Main.java
@@ -1,11 +1,24 @@
-
+import java.util.Scanner;
 
 public class Main {
+    final static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        System.out.print("> ");
+
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        System.out.println();
+        System.out.println("_________");
+        for(int i=0; i<3; i++){
+            System.out.print("| ");
+            for(int j=0; j<3; j++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.print("| \n");
+            //System.out.println();
+        }
+        System.out.println("_________");
+
 
     }
 }
