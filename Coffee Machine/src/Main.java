2d6406d0539cef34b92084be2625910b196a32e4
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-16 17:26:48 +0530

    stage 2 completed

--- Coffee Machine/src/Main.java
+++ Coffee Machine/src/Main.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 class Main{
+    final static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!
-                """);
+        System.out.print("Write how many cups of coffee you will need:\n> ");
+        int cup = sc.nextInt();
+        System.out.println("For "+ cup +" cups of coffee you will need:");
+        System.out.println(cup * 200 +" ml of water");
+        System.out.println(cup * 50 +" ml of milk");
+        System.out.println(cup * 15 +" g of coffee beans");
     }
 }
