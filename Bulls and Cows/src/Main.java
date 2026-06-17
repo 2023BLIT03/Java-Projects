25d6a4f24da04597289c1dfa73d6e7c3471ae2d2
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-17 20:01:32 +0530

    stage 2 completed

--- Bulls and Cows/src/Main.java
+++ Bulls and Cows/src/Main.java
@@ -1,18 +1,48 @@
 import java.util.Scanner;
 
 class Main{
+    final static Scanner sc = new Scanner (System.in);
+    static int bull = 0;
+    static int cow = 0;
+    static String code = "9305";
+    static String str = "";
+    static char[] arr;
+
+
     public static void main(){
-        System.out.println("""
-                The secret code is prepared: ****.
-                
-                Turn 1. Answer:
-                1234
-                Grade: None.
-                
-                Turn 2. Answer:
-                9876
-                Grade: 4 bulls.
-                Congrats! The secret code is 9876.
-                """);
+        System.out.print(">");
+        str = sc.next();
+        arr = str.toCharArray();
+        isBull();
+        isCow();
+        print();
+    }
+    static void isBull(){
+        for(int i=0; i < code.length(); i++) {
+            if (code.charAt(i) == (arr[i])) {
+                bull++;
+            }
+        }
+    }
+
+    static void isCow(){
+        for(int i=0; i < code.length(); i++){
+            for(int j=0; j < code.length(); j++){
+                if(i != j && code.charAt(i) == (arr[j]) && code.charAt(i) != (arr[i])){
+                    cow++;
+                }
+            }
+        }
+    }
+    static void print(){
+        if(bull>0 && cow>0){
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bull, cow, code);
+        }else if(bull>0){
+            System.out.printf("Grade: %d bull(s). The secret code is %s.\n", bull, code);
+        }else if(cow>0){
+            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cow, code);
+        }else{
+            System.out.printf("None. The secret code is %s.\n", code);
+        }
     }
 }
