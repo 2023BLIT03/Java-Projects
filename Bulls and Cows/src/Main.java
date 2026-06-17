7cbc9f1ae50a5f7b5c2d5f3f766fb0468e6d6172
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-17 18:45:29 +0530

    stage 1 completed

---
+++ Bulls and Cows/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(){
+        System.out.println("""
+                The secret code is prepared: ****.
+                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.
+                """);
+    }
+}
