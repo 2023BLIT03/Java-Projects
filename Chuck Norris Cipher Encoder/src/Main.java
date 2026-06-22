5e03dbdb47832cad0908d9974e56dfd25672d056
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-22 12:56:28 +0530

    stage 3 completed

--- Chuck Norris Cipher Encoder/src/Main.java
+++ Chuck Norris Cipher Encoder/src/Main.java
@@ -19,10 +19,30 @@ class Converter{
 
     static void encrypt(String str){
         for(int i=0; i < str.length(); i++){
-            System.out.println(str.charAt(i) + " = " + toBinary(str.charAt(i)));
+            String binary = toBinary(str.charAt(i));
+            grouping(binary);
+            //System.out.println(str.charAt(i) + " = " + binary);
         }
     }
     static String toBinary(char c){
         return Integer.toBinaryString(c);
+    }
+    static void grouping(String binary){
+        for(int i = 0; i < binary.length(); i++){
+            char current = binary.charAt(i);
+            int count = 0;
+
+            while(i < binary.length() && binary.charAt(i) == current){
+                count++;
+                i++;
+            }
+
+            if(current == '1'){
+                System.out.print("0 ");
+            }else{
+                System.out.print("00 ");
+            }
+             System.out.print("0".repeat(count) + " ");
+        }
     }
 }
