cc1894248808d6e0a7b1c109430f391f758e66be
MohdZeeshan <zeeshan3890s@gmail.com>
2026-06-24 16:44:35 +0530

    stage 3 completed

--- Rock-Paper-Scissors/src/Main.java
+++ Rock-Paper-Scissors/src/Main.java
@@ -10,39 +10,46 @@ public class Main{
     }
 
     static void input(){
-        IO.print(">");
-        String option = sc.next();
-        switch(option){
-            case "rock":
-                if(option.equals(compare())) {
-                    System.out.println("There is a draw (" + option + ")");
-                }else if("paper".equals(compare())) {
-                    System.out.println("Sorry, but the computer chose paper");
-                }else{
-                    System.out.println("Well done. The computer chose scissors and failed");
-                }
-                break;
-            case "paper":
-                if(option.equals(compare())) {
-                    System.out.println("There is a draw (" + option + ")");
-                }else if("scissors".equals(compare())) {
-                    System.out.println("Sorry, but the computer chose scissors");
-                }else{
-                    System.out.println("Well done. The computer chose rock and failed");
-                }
-                break;
-            case "scissors":
-                if(option.equals(compare())) {
-                    System.out.println("There is a draw (" + option + ")");
-                }else if("rock".equals(compare())) {
-                    System.out.println("Sorry, but the computer chose rock");
-                }else{
-                    System.out.println("Well done. The computer chose paper and failed");
-                }
-                break;
-            default:
-                System.out.println("Invalid Selection");
-        }
+        String option;
+        do{
+            IO.print(">");
+            option = sc.next();
+            switch(option){
+                case "rock":
+                    if(option.equals(compare())) {
+                        System.out.println("There is a draw (" + option + ")");
+                    }else if("paper".equals(compare())) {
+                        System.out.println("Sorry, but the computer chose paper");
+                    }else{
+                        System.out.println("Well done. The computer chose scissors and failed");
+                    }
+                    break;
+                case "paper":
+                    if(option.equals(compare())) {
+                        System.out.println("There is a draw (" + option + ")");
+                    }else if("scissors".equals(compare())) {
+                        System.out.println("Sorry, but the computer chose scissors");
+                    }else{
+                        System.out.println("Well done. The computer chose rock and failed");
+                    }
+                    break;
+                case "scissors":
+                    if(option.equals(compare())) {
+                        System.out.println("There is a draw (" + option + ")");
+                    }else if("rock".equals(compare())) {
+                        System.out.println("Sorry, but the computer chose rock");
+                    }else{
+                        System.out.println("Well done. The computer chose paper and failed");
+                    }
+                    break;
+                case "!exit":
+                    System.out.println("Bye!");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }while(!option.equals("!exit"));
+
     }
 
     enum Color{
